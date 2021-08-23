267a59949bfc19ec23dc3718bc9e425152cab352
Flavien Binet <flavien.binet@gmail.com>
2021-08-23 14:11:52 +0200

    chores: lint collect_staked.js

--- scripts/staking/collect_staked.js
+++ scripts/staking/collect_staked.js
@@ -1,56 +1,55 @@
-const Web3 = require('web3');
-const fs = require('fs');
-const { BN } = require('bn.js');
-const { Decimal } = require('decimal.js');
-const path = require('path');
-const EthUtil = require('ethereumjs-util')
-const EthTx = require('ethereumjs-tx')
-const { bech32 } = require('bech32')
-const { createHash } = require('crypto');
-const secp256k1 = require('secp256k1');
-const assert = require('assert');
+const Web3 = require("web3");
+const fs = require("fs");
+const { BN } = require("bn.js");
+const { Decimal } = require("decimal.js");
+const path = require("path");
+const EthUtil = require("ethereumjs-util");
+const EthTx = require("ethereumjs-tx");
+const { bech32 } = require("bech32");
+const { createHash } = require("crypto");
+const secp256k1 = require("secp256k1");
+const assert = require("assert");
 
 const decimalPrecision = 100;
-const fetErc20CanonicalMultiplier = new Decimal('1e18');
+const fetErc20CanonicalMultiplier = new Decimal("1e18");
 
 function canonicalFetToFet(canonicalVal) {
-    const origPrecision = Decimal.precision
-    Decimal.set({ precision: decimalPrecision })
+    const origPrecision = Decimal.precision;
+    Decimal.set({ precision: decimalPrecision });
     try {
-        return (new Decimal(canonicalVal.toString())).div(fetErc20CanonicalMultiplier)
-    }
-    finally {
-        Decimal.set({ precision: origPrecision })
+        return new Decimal(canonicalVal.toString()).div(
+            fetErc20CanonicalMultiplier
+        );
+    } finally {
+        Decimal.set({ precision: origPrecision });
     }
 }
 
 function fetToCanonicalFet(val) {
-    const origPrecision = Decimal.precision
-    Decimal.set({ precision: decimalPrecision })
+    const origPrecision = Decimal.precision;
+    Decimal.set({ precision: decimalPrecision });
     try {
-        return (new Decimal(val.toString())).mul(fetErc20CanonicalMultiplier)
-    }
-    finally {
-        Decimal.set({ precision: origPrecision })
+        return new Decimal(val.toString()).mul(fetErc20CanonicalMultiplier);
+    } finally {
+        Decimal.set({ precision: origPrecision });
     }
 }
 
 const dir = __dirname;
-const infuraProjectId = fs.readFileSync('.infura_project_id').toString().trim();
-const _endpoint = 'wss://mainnet.infura.io/ws/v3';
+const infuraProjectId = fs.readFileSync(".infura_project_id").toString().trim();
+const _endpoint = "wss://mainnet.infura.io/ws/v3";
 const endpoint = `${_endpoint}/${infuraProjectId}`;
 
 const web3http = new Web3(`https://mainnet.infura.io/v3/${infuraProjectId}`);
 
-const web3 = new Web3(new Web3.providers.WebsocketProvider(
-    endpoint,
-    {
+const web3 = new Web3(
+    new Web3.providers.WebsocketProvider(endpoint, {
         clientConfig: {
             maxReceivedFrameSize: 10000000000,
             maxReceivedMessageSize: 10000000000,
-        }
-    }));
-
+        },
+    })
+);
 
 class Contract {
     constructor(filename, address, startBlock = null, endBlock = null) {
@@ -60,7 +59,6 @@ class Contract {
         this.contract = new web3.eth.Contract(abi, address);
     }
 }
-
 
 class Asset {
     constructor(principal, compountInterest) {
@@ -81,7 +79,9 @@ class InterestRate {
     constructor(sinceBlock, ratePerBlockCanonical) {
         this.sinceBlock = new BN(sinceBlock); // necessary to avoid returning original `value` BN instance
         this.ratePerBlockCanonical = new BN(ratePerBlockCanonical);
-        this.ratePerBlock = canonicalFetToFet(this.ratePerBlockCanonical.toString());
+        this.ratePerBlock = canonicalFetToFet(
+            this.ratePerBlockCanonical.toString()
+        );
     }
 
     clone() {
@@ -100,8 +100,12 @@ class InterestRates {
     }
 
     static async queryFromContract() {
-        const startIdx = parseInt(await staking.contract.methods._interestRatesStartIdx().call());
-        const nextIdx = parseInt(await staking.contract.methods._interestRatesNextIdx().call());
+        const startIdx = parseInt(
+            await staking.contract.methods._interestRatesStartIdx().call()
+        );
+        const nextIdx = parseInt(
+            await staking.contract.methods._interestRatesNextIdx().call()
+        );
 
         let rates = {};
         for (let i = startIdx; i < nextIdx; ++i) {
@@ -125,7 +129,9 @@ class Stake {
     }
 
     static async queryFromContract(userAddress) {
-        const s = await staking.contract.methods.getStakeForUser(userAddress).call();
+        const s = await staking.contract.methods
+            .getStakeForUser(userAddress)
+            .call();
 
         const asset = new Asset(s[0], s[1]);
         return new Stake(asset, s[2], s[3]);
@@ -137,10 +143,14 @@ class Stake {
         let composite = new Decimal(this.asset.compositeFET);
         const _1 = new Decimal("1");
 
-        for (let i = this.sinceInterestRateIndex; i < interestRates.nextIdx; ++i) {
+        for (
+            let i = this.sinceInterestRateIndex;
+            i < interestRates.nextIdx;
+            ++i
+        ) {
             const n = i + 1;
             const rate = interestRates.ratesMap[i];
-            let startBlock = BN.max(this.sinceBlock, rate.sinceBlock)
+            let startBlock = BN.max(this.sinceBlock, rate.sinceBlock);
             let endBlock = untilBlock;
 
             if (n < interestRates.nextIdx) {
@@ -148,8 +158,12 @@ class Stake {
                 endBlock = BN.min(endBlock, nextRate.sinceBlock);
             }
 
-            const num_of_blocks = new Decimal(endBlock.sub(startBlock).toString());
-            const interestMultiplier = _1.add(rate.ratePerBlock).pow(num_of_blocks);
+            const num_of_blocks = new Decimal(
+                endBlock.sub(startBlock).toString()
+            );
+            const interestMultiplier = _1
+                .add(rate.ratePerBlock)
+                .pow(num_of_blocks);
             const accrued_composite = composite.mul(interestMultiplier);
             composite = accrued_composite;
         }
@@ -160,7 +174,11 @@ class Stake {
     }
 
     clone() {
-        return new Stake(this.asset, this.sinceBlock, this.sinceInterestRateIndex);
+        return new Stake(
+            this.asset,
+            this.sinceBlock,
+            this.sinceInterestRateIndex
+        );
     }
 }
 
@@ -174,22 +192,39 @@ class UserAssets {
     static async queryFromContract(userAddress) {
         const stake = await Stake.queryFromContract(userAddress);
 
-        const locA = await staking.contract.methods.getLockedAssetsAggregateForUser(userAddress).call();
+        const locA = await staking.contract.methods
+            .getLockedAssetsAggregateForUser(userAddress)
+            .call();
         const lockedAggr = new Asset(locA[0], locA[1]);
 
-        const liq = await staking.contract.methods._liquidity(userAddress).call();
+        const liq = await staking.contract.methods
+            ._liquidity(userAddress)
+            .call();
         const liquidity = new Asset(liq[0], liq[1]);
 
         return new UserAssets(stake, lockedAggr, liquidity);
     }
 
     calcCompoundInterest(interestRates, untilBlock) {
-        const compoundInterest_from_stake = this.stake.calcCompoundInterest(interestRates, untilBlock);
-        return [compoundInterest_from_stake.add(this.lockedAggr.compoundInterestFET).add(this.liquidity.compoundInterestFET), compoundInterest_from_stake];
+        const compoundInterest_from_stake = this.stake.calcCompoundInterest(
+            interestRates,
+            untilBlock
+        );
+        return [
+            compoundInterest_from_stake
+                .add(this.lockedAggr.compoundInterestFET)
+                .add(this.liquidity.compoundInterestFET),
+            compoundInterest_from_stake,
+        ];
     }
 
     calcPrincipal() {
-        return [this.stake.asset.principalFET.add(this.lockedAggr.principalFET).add(this.liquidity.principalFET), this.stake.asset.principalFET];
+        return [
+            this.stake.asset.principalFET
+                .add(this.lockedAggr.principalFET)
+                .add(this.liquidity.principalFET),
+            this.stake.asset.principalFET,
+        ];
     }
 
     clone() {
@@ -199,10 +234,12 @@ class UserAssets {
 
 class User {
     constructor(address, pubkey) {
-        const addrFromPubkey = EthUtil.pubToAddress('0x' + pubkey);
+        const addrFromPubkey = EthUtil.pubToAddress("0x" + pubkey);
         assert(
             addrFromPubkey.equals(EthUtil.toBuffer(address)),
-            `pubkey ${pubkey} does not match address, got ${addrFromPubkey.toString('hex')}, want ${address}.`
+            `pubkey ${pubkey} does not match address, got ${addrFromPubkey.toString(
+                "hex"
+            )}, want ${address}.`
         );
 
         this.address = address;
@@ -218,13 +255,15 @@ class User {
 
     async init(interestRates, untilBlock) {
         this.assets = await UserAssets.queryFromContract(this.address);
-        [this.principalFET_whole, this.principalFET_staked] = this.assets.calcPrincipal();
-        [this.compoundInterestFET_whole, this.compoundInterestFET_staked] = this.assets.calcCompoundInterest(interestRates, untilBlock);
+        [this.principalFET_whole, this.principalFET_staked] =
+            this.assets.calcPrincipal();
+        [this.compoundInterestFET_whole, this.compoundInterestFET_staked] =
+            this.assets.calcCompoundInterest(interestRates, untilBlock);
     }
 }
 
 async function getPublicKeyFromTxHash(txHash) {
-    const tx = await web3http.eth.getTransaction(txHash)
+    const tx = await web3http.eth.getTransaction(txHash);
     const txDetails = {
         nonce: tx.nonce,
         gasPrice: EthUtil.bufferToHex(new EthUtil.BN(tx.gasPrice)),
@@ -238,20 +277,33 @@ async function getPublicKeyFromTxHash(txHash) {
         v: tx.v,
     };
 
-    const txObj = new EthTx.Transaction(txDetails)
-    return txObj.getSenderPublicKey().toString('hex');
+    const txObj = new EthTx.Transaction(txDetails);
+    return txObj.getSenderPublicKey().toString("hex");
 }
 
 function secp256k1UncompressedPubkeyToFetchAddress(pubKeyHexStr) {
-    const binaryAddress = createHash('ripemd160').update(
-        createHash('sha256').update(
-            secp256k1.publicKeyConvert(Buffer.from('04' + pubKeyHexStr, 'hex'), true)
-        ).digest()).digest()
+    const binaryAddress = createHash("ripemd160")
+        .update(
+            createHash("sha256")
+                .update(
+                    secp256k1.publicKeyConvert(
+                        Buffer.from("04" + pubKeyHexStr, "hex"),
+                        true
+                    )
+                )
+                .digest()
+        )
+        .digest();
 
-    return bech32.encode('fetch', bech32.toWords(binaryAddress))
+    return bech32.encode("fetch", bech32.toWords(binaryAddress));
 }
 
-const staking = new Contract('Staking.json', '0x351baC612B50e87B46e4b10A282f632D41397DE2', 11061460, "latest");
+const staking = new Contract(
+    "Staking.json",
+    "0x351baC612B50e87B46e4b10A282f632D41397DE2",
+    11061460,
+    "latest"
+);
 
 async function main() {
     try {
@@ -260,10 +312,16 @@ async function main() {
         const curr_time = new Date();
         const end_time = new Date("2021-07-15T12:00:00Z");
         if (end_time < curr_time) {
-            console.error(`Current time ${curr_time} passed expected decommission time ${end_time} for FET Staking contract.`);
+            console.error(
+                `Current time ${curr_time} passed expected decommission time ${end_time} for FET Staking contract.`
+            );
         }
         const average_block_generation_time_secs = 13.17; // [sec/block]
-        const estimated_end_block = Math.ceil(current_block + (end_time - curr_time) / (average_block_generation_time_secs * 1000))
+        const estimated_end_block = Math.ceil(
+            current_block +
+                (end_time - curr_time) /
+                    (average_block_generation_time_secs * 1000)
+        );
 
         const interestRates = await InterestRates.queryFromContract();
 
@@ -275,8 +333,12 @@ async function main() {
         const staking_event_names = ["LiquidityDeposited"];
         for (let i = 0; i < staking_event_names.length; ++i) {
             const evt_name = staking_event_names[i];
-            events = await staking.contract.getPastEvents(evt_name, { fromBlock: staking.startBlock, toBlock: staking.endBlock });
-            retval.staking.events_list = retval.staking.events_list.concat(events);
+            events = await staking.contract.getPastEvents(evt_name, {
+                fromBlock: staking.startBlock,
+                toBlock: staking.endBlock,
+            });
+            retval.staking.events_list =
+                retval.staking.events_list.concat(events);
             const users_dict = retval.staking.users;
 
             for (let i = 0; i < events.length; ++i) {
@@ -290,14 +352,16 @@ async function main() {
                 if (e.returnValues.stakerAddress in users_dict) {
                     user = users_dict[userAddr];
                 } else {
-                    userPubkey = await getPublicKeyFromTxHash(e.transactionHash);
+                    userPubkey = await getPublicKeyFromTxHash(
+                        e.transactionHash
+                    );
                     user = new User(userAddr, userPubkey);
                     users_dict[userAddr] = user;
                 }
 
                 user.events.push(e);
             }
-        };
+        }
 
         //console.log(`USER ADDRESS, USER PUBKEY, USER FETCH ADDRESS, TOTAL[afet], PRINCIPAL WHOLE[afet], COMPOUND INTEREST(LIQUID + LOCKED + STAKED)[afet]`);
         for (const [key, user] of Object.entries(retval.staking.users)) {
@@ -306,7 +370,11 @@ async function main() {
             const principal = fetToCanonicalFet(user.principalFET_whole);
             const compound = fetToCanonicalFet(user.compoundInterestFET_whole);
 
-            console.log(`${key},${user.pubkey},${user.fetchAddr},${principal.add(compound).toFixed()},${principal.toFixed()},${compound.toFixed()}`);
+            console.log(
+                `${key},${user.pubkey},${user.fetchAddr},${principal
+                    .add(compound)
+                    .toFixed()},${principal.toFixed()},${compound.toFixed()}`
+            );
         }
     } finally {
         web3.currentProvider.connection.close();
