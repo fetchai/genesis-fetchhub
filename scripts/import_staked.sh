be4cea51d6e27801d93dd7f05c7b03808d631c0a
daeMOn <flavien.binet@gmail.com>
2021-09-14 15:25:50 +0200

    feat: genesis migration docs and scripts (#1)
    * feat: genesis migration docs and scripts
    
    * fix: add ETH pubkey/address check
    
    * fix: shellcheck + error handling in bash scripts
    
    * docs: clarify FETCHD_HOME usage
    
    * fix: improve import script logging
    
    * fix: remove bc deps from import_staked.sh
    
    * fix: improve import_staked.sh logging
    
    * fix: import_staked.sh format numbers properly
    
    * fix: add gawk requirement to import_staked.sh
    
    * docs: add last migration steps
    
    * fix: sort need fixed locale to be consistent
    
    * fix: fixed local sorted staking data
    
    * fix: swap staked collection event from BindStake to LiquidityDeposited
    
    * chores: lint collect_staked.js
    
    * docs: updates & cleanup
    
    * fix: handle txs from london fork on collect_staked.js
    
    * fix: bump fetchd version
    
    * chores: bump fetchd version
    
    * fix: update target height to stop the network
    
    * fix: update start block height and add mobx account address
    
    * fix: setting initial mobx tokens on existing foundation account
    
    * feat: do not create delegations from import_staked script
    
    * Fix integer idx handling in rates loop (#4)
    
    * Fix for integer idx hangling in rates loop
    
    * Carving the end block high (13224000) in to the stone
    
    Co-authored-by: void* <peter.bukva@fetch.ai>

---
+++ scripts/import_staked.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+set -uoe pipefail
+
+usage() {
+    echo "Add delegations in the genesis file from a CSV file."
+    echo "The genesis from FETCHD_HOME (default: ~/.fetchd/) will be updated."
+    echo -e "\nUsage:\n    $0 [csv_file]\n" 
+    echo -e "Env:"
+    echo -e "    FETCHD_HOME: specify the location of the fetchd home folder\n"
+    exit 1
+}
+
+# some systems default to mawk, which can't do 64bits maths, so make sure gawk is here.
+which gawk >/dev/null 2>&1 || (echo "gawk is required and not installed. Install it with 'sudo apt-get install gawk'" && exit 1)
+
+FETCHD_HOME="${FETCHD_HOME:=~/.fetchd/}"
+# Validators that will receive delegations, in a round-robin fashion
+TARGET_VALIDATORS=(
+    "fetchvaloper1xrlqqepyfl64mjr8x837y5slxfzlywe57rwdta" # novus1
+    "fetchvaloper1w8659233jlf42n68xsqw9w3ssjquyr8jha64hy" # novus2
+    "fetchvaloper14w6a4al72uc3fpfy4lqtg0a7xtkx3w7hda0vel" # uvue1
+    "fetchvaloper1ufarthku3aw4rjeexdwk9r2wj20evdnw86ceee" # uvue2
+)
+BOND_DENOM="afet"
+# minimum amount required to create a delegation, below, no delegation is 
+# created and everything is transferred on the user account
+# value is in units of BOND_DENOM
+# 2021-09-13 update:
+# it was decided to NOT create any delegation but just transfer all funds 
+# to the users wallets. The min-delegation is now set to the total supply.
+MIN_DELEGATED_AMOUNT="1067305583779492842119609694"
+# amount subtracted from the delegated amount and transferred 
+# on the user account when a delegation is created 
+# value is in units of BOND_DENOM
+ACCOUNT_RESERVED_AMOUNT="1000000000000000000"
+# Number of expected fields per line in the CSV file
+EXPECTED_NUMFIELDS="6"
+
+if [ $# -ne 1 ]; then
+    usage
+    exit 1
+fi
+
+CSV_FILE="$1"
+
+if [ ! -f "${CSV_FILE}" ]; then
+    usage
+    exit 1
+fi
+
+NUMFIELDS=$(head -n1 "${CSV_FILE}" | gawk -F',' '{print NF}')
+if [ "${NUMFIELDS}" -ne "${EXPECTED_NUMFIELDS}" ]; then 
+    echo "invalid CSV file, expected ${EXPECTED_NUMFIELDS}, got ${NUMFIELDS}"
+    exit 1
+fi
+
+FETCHD_HOME=$(realpath "${FETCHD_HOME}")
+GENESIS_FILE="${FETCHD_HOME}/config/genesis.json"
+if [ ! -f "${GENESIS_FILE}" ]; then
+    echo "Cannot read genesis.json from ${FETCHD_HOME}"
+    exit 1
+fi
+
+ORIGINAL_GENESIS="${GENESIS_FILE}.orig"
+# Backup current genesis. In case of errors while adding delegations,
+# genesis file will be reverted to pristine state.
+cp -f "${GENESIS_FILE}" "${ORIGINAL_GENESIS}"
+
+restore () {
+    echo "An error occured, reverting the genesis.json to previous state"
+    cp -f "${ORIGINAL_GENESIS}" "${GENESIS_FILE}"
+}
+cleanup () {
+    rm "${ORIGINAL_GENESIS}"
+}
+
+trap restore ERR
+trap cleanup EXIT
+
+COUNTER=0
+while read -r line; do
+    FETCH_ADDR=$(echo "${line}" | gawk -F',' '{print $3}')
+    VALIDATOR=${TARGET_VALIDATORS[$((COUNTER % ${#TARGET_VALIDATORS[@]}))]}
+    AMOUNT=$(echo "${line}" | gawk -F',' '{print $4}')
+    fetchd add-genesis-delegation \
+        --home "${FETCHD_HOME}" \
+        --account-reserved-amount "${ACCOUNT_RESERVED_AMOUNT}${BOND_DENOM}" \
+        --min-delegated-amount "${MIN_DELEGATED_AMOUNT}${BOND_DENOM}" \
+        "${FETCH_ADDR}" "${VALIDATOR}" "${AMOUNT}${BOND_DENOM}" 
+    if [[ `echo "${AMOUNT} ${MIN_DELEGATED_AMOUNT}" | gawk '{print ($1 >= $2)}'` == 1 ]]; then
+        echo "Added ${ACCOUNT_RESERVED_AMOUNT}${BOND_DENOM} to ${FETCH_ADDR}"
+        echo "Delegated $(echo "${AMOUNT} ${ACCOUNT_RESERVED_AMOUNT}" | gawk '{printf "%.0f", $1-$2}')${BOND_DENOM} from ${FETCH_ADDR} to ${VALIDATOR}"
+        COUNTER=$((COUNTER + 1))
+    else
+        echo "Added ${AMOUNT%.*}${BOND_DENOM} to ${FETCH_ADDR}"
+    fi
+done <<<"$(LC_ALL=C sort "${CSV_FILE}")"
---
+++ scripts/staking/README.md
@@ -0,0 +1,39 @@
+# Staked tokens collection script
+
+Export currently staked ERC20 tokens with calculated compound interest. Also compute the fetch address from the ETH public key.
+
+## Requirements
+
+- node >= v16.0
+- Infura Ethereum API project ID [https://infura.io/dashboard/ethereum](https://infura.io/dashboard/ethereum)
+
+> The free tier account on Infura is enough for exporting the data, the script needs ~8200 API request from the 100k quota.
+
+## Collect staked tokens
+
+First, create a `.infura_project_id` file in this folder, with your Infura Ethereum API project ID in it.
+Then execute:
+
+```bash
+# install dependencies
+npm install
+
+# collect staked tokens and accounts
+node collect_staked.js > staked_export.csv
+```
+
+This will run for *~25 minutes*, and create a `staked_export.csv` file will all currently staked tokens and accounts.
+
+The CSV file will contains the following fields on each line:
+
+```
+USER ADDRESS, USER PUBKEY, USER FETCH ADDRESS, TOTAL[afet], PRINCIPAL WHOLE[afet], COMPOUND INTEREST(LIQUID + LOCKED + STAKED)[afet]
+```
+
+Keep this `staked_export.csv` file path as this will be the input required to add the genesis delegations later.
+
+You can verify you have the correct file by hashing it and comparing with our provided hash. Remember to sort it first.
+
+```bash
+cat staked_export.csv | LC_ALL=C sort | sha256sum
+```
